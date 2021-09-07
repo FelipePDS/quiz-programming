25cf61019919bbd9c1185c567f2d7ee67d8c14c0
FelipePDS <felipepdasilva66@gmail.com>
2021-09-06 22:47:18 -0300

    remove gitignore

---
+++ assets/scripts/local/data.js
@@ -0,0 +1,291 @@
+export const questions = [
+  {
+    id: 1,
+    value: 'Qual operador aritmético é usado para calcular o resto da divisão?',
+    correctAnswerId: 'b',
+    userAnswerId: '',
+    
+    alternatives: [
+      {
+        id: 'a',
+        value: 'Operador: /'
+      },
+
+      {
+        id: 'b',
+        value: 'Operador: %'
+      },
+
+      {
+        id: 'c',
+        value: 'Operador: **'
+      },
+
+      {
+        id: 'd',
+        value: 'Operador %%'
+      }
+    ]
+  },
+
+  {
+    id: 2,
+    value: 'O que é uma variável na programação?',
+    correctAnswerId: 'a',
+    userAnswerId: '',
+    
+    alternatives: [
+      {
+        id: 'a',
+        value: 'Um campo no sistema para armazenar dados'
+      },
+
+      {
+        id: 'b',
+        value: 'Um algoritmo que calcula operações aritméticas'
+      },
+
+      {
+        id: 'c',
+        value: 'Um algoritmo para calcular operações lógicas'
+      },
+
+      {
+        id: 'd',
+        value: 'Um comando para executar algum evento ou ação'
+      }
+    ]
+  },
+
+  {
+    id: 3,
+    value: 'Qual dos paradigmas abaixo não é usado na programação?',
+    correctAnswerId: 'b',
+    userAnswerId: '',
+    
+    alternatives: [
+      {
+        id: 'a',
+        value: 'Orientado a Objetos'
+      },
+
+      {
+        id: 'b',
+        value: 'Linear'
+      },
+
+      {
+        id: 'c',
+        value: 'Funcional'
+      },
+
+      {
+        id: 'd',
+        value: 'Lógico'
+      }
+    ]
+  },
+
+  {
+    id: 4,
+    value: 'O que é, ou para que serve uma classe?',
+    correctAnswerId: 'c',
+    userAnswerId: '',
+    
+    alternatives: [
+      {
+        id: 'a',
+        value: 'Um tipo de variável'
+      },
+
+      {
+        id: 'b',
+        value: 'Um objeto com o objetivo de armazenar apenas metódos / funções'
+      },
+
+      {
+        id: 'c',
+        value: 'Ela é composta por atributos e/ou metódos para estruturar um novo objeto'
+      },
+
+      {
+        id: 'd',
+        value: 'Ela define apenas tipos de dados para os atributos de um objeto'
+      }
+    ]
+  },
+
+  {
+    id: 5,
+    value: 'Qual o objetivo de declarar um Interface?',
+    correctAnswerId: 'b',
+    userAnswerId: '',
+    
+    alternatives: [
+      {
+        id: 'a',
+        value: 'Declarar metódos como atributos de um objeto'
+      },
+
+      {
+        id: 'b',
+        value: 'Declarar tipos de dados para atributos e/ou definir os parâmetros de uma interface e o que ele retorna. Para estruturar um objeto/classe'
+      },
+
+      {
+        id: 'c',
+        value: 'Atribuir dados para atributos de uma classe'
+      },
+
+      {
+        id: 'd',
+        value: 'Instanciar uma Classe para um novo objeto'
+      }
+    ]
+  },
+
+  {
+    id: 6,
+    value: 'O que é um array?',
+    correctAnswerId: 'a',
+    userAnswerId: '',
+    
+    alternatives: [
+      {
+        id: 'a',
+        value: 'É um tipo de variável que armazena uma lista de dados'
+      },
+
+      {
+        id: 'b',
+        value: 'É um inicializador de um novo objeto'
+      },
+
+      {
+        id: 'c',
+        value: 'É um método para instanciar uma nova classe'
+      },
+
+      {
+        id: 'd',
+        value: 'É uma formatação de algoritmo'
+      }
+    ]
+  },
+
+  {
+    id: 7,
+    value: 'Qual dos tipos de dados a seguir não está correto?',
+    correctAnswerId: 'd',
+    userAnswerId: '',
+    
+    alternatives: [
+      {
+        id: 'a',
+        value: 'String'
+      },
+
+      {
+        id: 'b',
+        value: 'Integer'
+      },
+
+      {
+        id: 'c',
+        value: 'Boolean'
+      },
+
+      {
+        id: 'd',
+        value: 'Void'
+      }
+    ]
+  },
+
+  {
+    id: 8,
+    value: 'Qual a função de um Construtor em uma Classe?',
+    correctAnswerId: 'b',
+    userAnswerId: '',
+    
+    alternatives: [
+      {
+        id: 'a',
+        value: 'Indicar o tipo de dado no retorno dos métodos'
+      },
+
+      {
+        id: 'b',
+        value: 'Instanciar uma classe para um novo objeto'
+      },
+
+      {
+        id: 'c',
+        value: 'Agrupar um conjunto de operações condicionais a partir de um dado'
+      },
+
+      {
+        id: 'd',
+        value: 'Declarar os tipos de dados que a classe vai extender'
+      }
+    ]
+  },
+
+  {
+    id: 9,
+    value: 'Qual o objetivo do termo "Concatenar"?',
+    correctAnswerId: 'c',
+    userAnswerId: '',
+    
+    alternatives: [
+      {
+        id: 'a',
+        value: 'Definir um tipo de dado para uma variável'
+      },
+
+      {
+        id: 'b',
+        value: 'Abstrair uma String em duas partes'
+      },
+
+      {
+        id: 'c',
+        value: 'Juntar dados para formar um tipo de dado em si'
+      },
+
+      {
+        id: 'd',
+        value: 'Definir uma sequência de variáveis'
+      }
+    ]
+  },
+
+  {
+    id: 10,
+    value: 'Qual dos operadores lógicos a seguir não é usado na programação?',
+    correctAnswerId: 'd',
+    userAnswerId: '',
+    
+    alternatives: [
+      {
+        id: 'a',
+        value: '||'
+      },
+
+      {
+        id: 'b',
+        value: '=='
+      },
+
+      {
+        id: 'c',
+        value: '!'
+      },
+
+      {
+        id: 'd',
+        value: '?'
+      }
+    ]
+  }
+];
